2bc263e2ccbd8ce26f5420f077b8c4e26046c6f9
hasanRaza06 <128395747+hasanRaza06@users.noreply.github.com>
2023-11-07 19:44:54 +0530

    Update bill.c

--- bill.c
+++ bill.c
@@ -1,7 +1,1 @@
-#include<stdio.h>
-void main()
-{
- int i,j,res;
- printf("Wlecome to our shop what you want to buy choose items : \n");
- printf("Press 1 for 'samsung' products :\n Press 2 for 'redmi' products :\n   Press 3 for 'realme' products :\n  ") 
- switch(res)
+
