3504dfcc796a716d487636ce775727b52650fd97
hasanRaza06 <128395747+hasanRaza06@users.noreply.github.com>
2023-11-07 19:39:02 +0530

    Add files via upload

---
+++ bill.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+ int i,j,res;
+ printf("Wlecome to our shop what you want to buy choose items : \n");
+ printf("Press 1 for 'samsung' products :\n Press 2 for 'redmi' products :\n   Press 3 for 'realme' products :\n  ") 
+ switch(res)
