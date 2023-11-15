b25c621e2129a487424ae7aeaf3f21cbeccf3f03
hasanRaza06 <128395747+hasanRaza06@users.noreply.github.com>
2023-11-15 23:46:33 +0530

    Add files via upload

---
+++ detail.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int father,dob,name,addres,hobbies,res;
+printf("My details :\n");
+name=Hasan raza;
+father=Amirullah Ansari;
+dob=6th,april,2002;
+addres=Siwankala,Sikanderpur,Ballia,(U.P);
+hobbies=cube,hacking,games;
+scanf("%d",&res);
+if(res=name)
+printf("%d",name);
+else if()
+
+}
